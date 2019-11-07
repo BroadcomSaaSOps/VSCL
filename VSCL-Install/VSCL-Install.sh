d446bbf7a52cf082a5a49dec07268fc5e507bd11
tayni03 <37160098+tayni03@users.noreply.github.com>
2019-11-07 09:17:24 -0800

    Update VSCL-Install.sh

--- VSCL-Install/VSCL-Install.sh
+++ VSCL-Install/VSCL-Install.sh
@@ -2,11 +2,16 @@
 
 #=============================================================================
 # NAME:     VSCL-Install.sh
+#-----------------------------------------------------------------------------
 # Purpose:  Installer for McAfee VirusScan Command Line Scanner 6.1.3 on 
 #           FedRAMP PPM App Servers
+#-----------------------------------------------------------------------------
 # Creator:  Nick Taylor, Pr. Engineer, Broadcom SaaS Ops
+#-----------------------------------------------------------------------------
 # Date:     21-OCT-2019
+#-----------------------------------------------------------------------------
 # Version:  1.2
+#-----------------------------------------------------------------------------
 # PreReqs:  Linux
 #           PPM Application Server
 #           ClamAV antivirus scanner installed and integrated with PPM
@@ -16,8 +21,11 @@
 #           VSCL-Download-DAT.sh in directory
 #           uvwrap.sh in directory
 #           unzip, tar, gunzip, gclib (> 2.7) utilities in OS
+#-----------------------------------------------------------------------------
 # Params:   none
+#-----------------------------------------------------------------------------
 # Switches: none
+#-----------------------------------------------------------------------------
 # Imports:  ./VSCL-local.sh
 #============================================================================= 
 
@@ -25,10 +33,12 @@
 # VARIABLES
 #=============================================================================
 
+#-----------------------------------------
 #  Imports
 #-----------------------------------------
 . ./VSCL-local.sh
 
+#-----------------------------------------
 # Globals
 # (these variables are normally best left unmodified)
 #-----------------------------------------
@@ -78,14 +88,21 @@ function Exit-Script {
     # Params: $1 = exit code (assumes 0/ok)
     #----------------------------------------------------------
 
+    local OUTCODE
+
     Log-Print "==========================="
 
-    if [ "$1" != "0" ]; then
-      Log-Print "Ending with error code: $1"
-      Log-Print "==========================="
+    if [[ -z "$1" ]]; then
+        OUTCODE="0"
+    else
+        if [ "$1" != "0" ]; then
+            OUTCODE="$1"
+        fi
     fi
-
-    exit $1
+    
+    Log-Print "Ending with exit code: $1"
+    Log-Print "==========================="
+    exit OUTCODE
 }
 
 function Exit-WithError() {
@@ -111,28 +128,34 @@ Log-Print "Beginning VSCL installation"
 Log-Print "==========================="
 
 # make temp directory off installer directory
-if [ ! -d "./$TEMP_DIR" ]; then 
+if [[ ! -d "./$TEMP_DIR" ]]; then 
   Log-Print "Creating temp directory '$TEMP_DIR'..."
   mkdir -p "./$TEMP_DIR"
 fi
 
 # move install files to unzip into temp
-# DO NOT quote below, [ -f ] conditionals don't work with quoting
-if [ -f ./$INSTALLER_ZIP ]; then
+# >>> DO NOT add quote below, [ -f ] conditionals don't work with quoting
+if [[ -f ./$INSTALLER_ZIP ]]; then
     Log-Print "Copying install archive to temp directory..."
     cp -f "./$INSTALLER_ZIP" "./$TEMP_DIR"
 else 
-    Log-Print "ERROR: Installer archive './$INSTALLER_ZIP' does not exist!"
-    Exit-Script 1
+    Exit-WithError "ERROR: Installer archive './$INSTALLER_ZIP' does not exist!"
 fi
 
 #TODO: Download installer from EPO
 
 # untar installer archive in-place and install uvscan with default settings
-Log-Print "Installing VSCL..."
+Log-Print "Extracting installer to directory './$TEMP_DIR'..."
 cd "./$TEMP_DIR"
-tar -xvzf "./$INSTALLER_ZIP"
-"./$INSTALL_CMD" -y
+
+if ! tar -xvzf "./$INSTALLER_ZIP"; then
+    Exit-WithError "Error extracting installer to directory './$TEMP_DIR'!"
+fi
+
+Log-Print "Installing VSCL..."
+if ! "./$INSTALL_CMD" -y; then
+    Exit-WithError "Error installing VirusScan Command Line Scanner!"
+fi
 
 # remove temp directory
 Log-Print "Removing temp directory..."
@@ -154,7 +177,10 @@ fi
 Log-Print "Setting up shim wrapper for uvscan..."
 chmod +x "./$WRAPPER"
 
-if [ -f "$UVSCAN_HOME/$WRAPPER" ]; then rm -f "$UVSCAN_HOME/$WRAPPER"; fi
+if [ -f "$UVSCAN_HOME/$WRAPPER" ]; then
+    rm -f "$UVSCAN_HOME/$WRAPPER"
+fi
+
 cp -f "./$WRAPPER" "$UVSCAN_HOME"
 
 if [ ! -d "$CLAMAV_HOME" ]; then
