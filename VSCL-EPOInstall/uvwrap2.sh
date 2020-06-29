10485d1e0c8f86502076d1496f2fa6e517869c04
Nicholas Taylor <37160098+tayni03@users.noreply.github.com>
2020-06-29 14:06:44 +0000

    Update uvwrap2.sh

--- VSCL-EPOInstall/uvwrap2.sh
+++ VSCL-EPOInstall/uvwrap2.sh
@@ -3,12 +3,12 @@
 #=============================================================================
 # NAME:     UVWRAP.SH
 #-----------------------------------------------------------------------------
-# Purpose:  __vscl_wrapper to redirect PPM command line antivirus call to McAfee 
-#           VirusScan Command line Scanner
+# Purpose:  VSCL uvscan wrapper to redirect PPM command line antivirus call to
+#           McAfee VirusScan Command line Scanner
 #-----------------------------------------------------------------------------
 # Creator:  Nick Taylor, Pr. Engineer, Broadcom SaaS Ops
 #-----------------------------------------------------------------------------
-# Date:     13-Feb-2020
+# Date:     18-Jun-2020
 #-----------------------------------------------------------------------------
 # Version:  1.25
 #-----------------------------------------------------------------------------
@@ -17,7 +17,7 @@
 #           VSCL installed and integrated with PPM
 #           unzip, tar, gunzip, gclib > 2.7 utilities in OS
 #-----------------------------------------------------------------------------
-# Params:   $1 = full path of file to be scanned (supplied by PPM) 
+# Params:   $1 = full path of file to be scanned (supplied by PPM)
 #-----------------------------------------------------------------------------
 # Switches: none
 #-----------------------------------------------------------------------------
@@ -30,47 +30,38 @@
 #=============================================================================
 # PREPROCESS: Prevent file from being sourced
 #=============================================================================
-# shellcheck disable=SC2091
 if $(return 0 2>/dev/null); then
     # File is  sourced, return error
-    echo "VSCL EPO Installer must NOT be sourced.  It must be run standalone!"
+    echo "VSCL uvscan wrapper must NOT be sourced.  It must be run standalone!"
     return 1
 fi
 
 #=============================================================================
 #  IMPORTS: Import any required libraries/files
 #=============================================================================
-
 unset include_path this_file
 declare include_path this_file
 
-# get this script's filename from bash
 this_file="${BASH_SOURCE[0]}"
-# bash_source does NOT follow symlinks, traverse them until we get a real file
-this_file=$(while [[ -L "$this_file" ]]; do 
-                this_file="$(readlink "$this_file")";
-                done; 
-                echo "$this_file")
-# extract path to this script
-include_path="${this_file%/*}"
-# shellcheck source=./VSCL-lib.sh
-. "$include_path/VSCL-lib.sh"
+
+# follow symlinks until real file found
+this_file=$(while [[ -L "$this_file" ]]; do this_file="$(readlink "$this_file")"; done; echo $this_file)
 
 #=============================================================================
 # GLOBALS: Global variables
 #=============================================================================
 # Abbreviation of this script name for logging
-
-declare -x __vscl_script_abbr="VSCLUVWRAP"
+# shellcheck disable=SC2034
+declare -x __vscl_script_abbr="VSCLUVWR"
 
 # Path to common log file for all VSCL scripts
 declare -x __vscl_log_path="/var/McAfee/agent/logs/VSCL_mgmt.log"
 
 # Options passed to VSCL scanner
 # -c                    clean viruses if found
-# -p                    
+# -p
 # --afc 512             half-meg buffers
-# -e                    
+# -e
 # --nocomp              don't scan compressed files
 # --ignore-links        ignore symlinks
 # --noboot              don't scan boot sectors
@@ -78,8 +69,7 @@ declare -x __vscl_log_path="/var/McAfee/agent/logs/VSCL_mgmt.log"
 # --noexpire            no error for expired files
 # --one-file-system     do not follow mouned directory trees
 # --timeout 10          scan for 10 seconds then abort
-SCAN_OPTIONS="-c -p --afc 512 -e --nocomp --ignore-links --noboot --nodecrypt --noexpire --one-file-system --timeout 10"
-
+declare -x SCAN_OPTIONS="-c -p --afc 512 -e --nocomp --ignore-links --noboot --nodecrypt --noexpire --one-file-system --timeout 10"
 
 #=============================================================================
 # FUNCTIONS: VSCL Library functions
@@ -89,12 +79,13 @@ function log_print {
     #----------------------------------------------------------
     # Print a message to the log defined in $__vscl_log_path
     # (by default '/var/McAfee/agent/logs/VSCL_mgmt.log')
+    # replaces same function from VSCL library
     #----------------------------------------------------------
     # Params: $1 = error message to print
     #----------------------------------------------------------
 
-    declare out_text 
-        
+    declare out_text
+
     # Prepend date/time, which script, then the log message
     # i.e.  "11/12/2019 11:14:10 AM:VSCL_UP1:[x]Refreshing agent data with EPO..."
     #        <- date -------------> <script> <+><-- message -->
@@ -103,17 +94,14 @@ function log_print {
 
     if [[ -w $__vscl_log_path ]]; then
         # log file exists and is writable, append
-        #echo -e "$out_text" | tee --append "$__vscl_log_path"
         printf "%s\n" "$out_text" | tee --append "$__vscl_log_path"
     else
         # log file absent, create
-        #echo -e "$OUTPUT" | tee "$__vscl_log_path"
         printf "%s\n" "$out_text" | tee "$__vscl_log_path"
     fi
-    
+
     return 0
 }
-
 
 #=============================================================================
 # MAIN: Code execution begins here
@@ -133,6 +121,8 @@ fi
 
 # call uvscan
 # shellcheck disable=2086
+sleep 2
+
 if [[ ! -f "$check_file" ]]; then
     log_print "[E]*** File Missing / Virus found! ***"
     exit 1
